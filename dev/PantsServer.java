01f694acde9caa6e495ceaa6603ee4afe9dd80a4
phase <ph4sesaber@gmail.com>
2015-10-18 21:54:30 -0700

    Get IPs from clients

--- dev/PantsServer.java
+++ dev/PantsServer.java
@@ -3,11 +3,19 @@
 public class PantsServer:
 
     final static ServerSocket server = new ServerSocket(444);
-    static ArrayList<Socket> connectedClients = new ArrayList<>();
+    static HashMap<String, Socket> connectedClients = new HashMap<>();
 
     public static void main(...):
         while(true):
             Socket client = server.accept();
-            connectedClients.add(client);
-            DataOutputStream outToClient = new DataOutputStream(server.getOutputStream());
-            outToClient.writeBytes("Connected!\n");
+            addClient(client);
+
+    public staic void addClient(Socket client):
+        BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        PrintStream toClient = new PrintStream(client.getOutputStream());
+        String in = fromClient.readline();
+        if(in.startsWith("cmd::")) runCommand(client, in.replaceFirst("cmd::", ""));
+            
+    public static void runCommand(Socket client, String cmd):
+        if(cmd.startsWith("set ")):
+            connectedClients.put(cmd.replaceFirst("set ", ""), client);
