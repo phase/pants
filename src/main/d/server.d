c2c0fb194de8653586e00761ae43102674330be2
phase <ph4sesaber@gmail.com>
2015-10-24 11:16:09 -0700

    server: store Ips from clients
    When the client handshakes with the server, it will send its Bukkit server's IP. This will put that, and the socket itself, in an associative array (HashMap) which we can use later.

--- src/main/d/server.d
+++ src/main/d/server.d
@@ -6,29 +6,41 @@ void main() {
     listener.bind(new InternetAddress("localhost", 444));
     listener.listen(10);
     auto readSet = new SocketSet();
-    Socket[] connectedClients;
+
+    string[Socket] connectedClients;
     Socket selectedClient;
     char[1024] buffer;
     bool isRunning = true;
+
     while(isRunning) {
         readSet.reset();
         readSet.add(listener);
+
         foreach(client; connectedClients) readSet.add(client);
+
         if(Socket.select(readSet, null, null)) {
-            foreach(client; connectedClients)
+            foreach(client; connectedClients.keys)
                 if(readSet.isSet(client)) {
+                    string ip = connectedClients[client];
+
                     // Read from it and echo it back
                     auto got = client.receive(buffer);
-                    writeln(buffer[0..got]);
+                    string out = "[" ~ ip ~ "] " ~ buffer[0..got];
+                    writeln(out);
                 }
             if(readSet.isSet(listener)) {
                 // The listener is ready to read, which means
                 // a new client wants to connect. We accept it here.
-                auto newSocket = listener.accept();
-                newSocket.send("Hello!\n"); // say hello
-                connectedClients ~= newSocket; // add to our list
+                auto client = listener.accept();
+
+                auto got = client.recieve(buffer); // Get Ip
+                string ip = buffer[0..got];
+                string out = ip ~ " connected!";
+                writeln(out);
+
+                connectedClients[client] = ip; // Add to the associative array
                 if(selectedClient is null) {
-                    selectedClient = newSocket;
+                    selectedClient = client;
                 }
             }
         }
